65ef2895e8dceb48705298e2e9bfe1d4ea66b2d1
EloyPardo <pardogurreaeloy@gmail.com>
2024-11-08 10:45:30 +0100

    A8 -> Continuada

--- Tema4/Practica1/a8.js
+++ Tema4/Practica1/a8.js
@@ -16,6 +16,26 @@ class Client {
 }
 class Bill {
 
-    constructor(clientID, ) {}
+    static #id = 1;
+
+    constructor(clientID, totalImport, isPaid) {
+        this.id = Bill.#id++;
+        this.clientID = clientID;
+        this.totalImport = totalImport;
+        this.isPaid = isPaid;
+    }
+
+    showBill(){
+        return `ID del cliente: ${this.clientID}, Importe total de la factura: ${this.totalImport}€, Estado: ${this.isPaid}`
+    }
+
+    payBill(){
+        if (!this.isPaid) {
+            this.isPaid = true;
+            console.log(`Factura con id: ${Bill.#id} está pagada.`)
+        } else {
+            console.log(`Esta factura ya esta pagada`)
+        }
+    }
 
 }
