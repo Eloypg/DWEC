aa079d30e3c7174507b1865038c61822cb166fb3
Eloypg <pardogurreaeloy@gmail.com>
2024-12-02 19:48:41 +0100

    Reto1 -> Acabado

--- Tema5/Reto1/script.js
+++ Tema5/Reto1/script.js
@@ -1,15 +1,37 @@
-function readParagraph() {
-    let email = document.getElementById("emailInput")[0].value;
-    const emailRegex = /^[a-z][a-z0-9\._%+\-]+@[a-z]+\.[a-z]{2,3}$/;
-    const errorMensaje = document.getElementById("errorMessage")
-
-    if(!email === null){
-        if(!emailRegex.test(email)) {
-            
-        }
-    } else {
-        console.log("El email es null");
-    }
-    
-    
+window.onload = function() {
+    const form = document.getElementById("addForm");
+    form.reset();
 }
+
+
+function readParagraph(){
+    const emailRegex = /^[a-z][a-z0-9_.]*@[a-z]+\.[a-z]{2,3}$/;
+
+    const email = document.getElementById("emailInput");
+    const emailErrMsg = document.getElementById("emailErrMsg");
+    const paragraphInput = document.getElementById("paragraphInput");
+
+    if(!emailRegex.test(email.value)) {
+        emailErrMsg.style.display = "inline";
+        emailErrMsg.textContent = "*El email no es correcto";
+    } else {
+        emailErrMsg.style.display = "none";
+        addParagraph(paragraphInput.value);
+    }
+}
+
+function addParagraph(paragraphText) {
+    const column1 = document.getElementById("column1");
+    const column2 = document.getElementById("column2");
+
+    const newParagraph = document.createElement("p");
+    newParagraph.innerHTML = `<strong>${paragraphText}</strong>`;
+    newParagraph.style.color = "red";
+
+    column1.appendChild(newParagraph);
+    const cloneParagraph = newParagraph.cloneNode(true); //Clona el parrafo con su contenido
+    cloneParagraph.style.color = "green";
+    column2.appendChild(cloneParagraph);
+}
+
+document.getElementById("addButton").addEventListener("click", readParagraph);
